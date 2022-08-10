179d6f4eed31b48eb0ed009b22f3b5acca93404b
unknown <khrulamn@gmail.com>
2022-08-10 13:57:53 +0800

    yes

--- frontEnd/game-guys/src/views/userReviews/UserReviews.js
+++ frontEnd/game-guys/src/views/userReviews/UserReviews.js
@@ -57,7 +57,7 @@ export default function UserReviews() {
         navigate('/')
     }
 
-    console.log('item',item)
+    console.log('item', item)
 
     return (
         <div className="bg-secondaryColor min-h-[calc(100vh-160px)] font-main">
@@ -96,8 +96,8 @@ export default function UserReviews() {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                                 </svg>
                                 <h3 className="text-white m-2 text-lg text-center">
-                                    No user reviews yet. <br/> To review, you must purchase the item first <br/>and proceed to shopping history.
-                                    </h3>
+                                    No user reviews yet. <br /> To review, you must purchase the item first <br />and proceed to shopping history.
+                                </h3>
                             </div>
                         }
                     </div>
